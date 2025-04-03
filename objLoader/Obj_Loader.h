d911886496e4413661c0f96a502da44a64afc017
Dunnnan <777dunny@gmail.com>
2025-04-03 18:11:56 +0200

    File reduction

--- objLoader/Obj_Loader.h
+++ objLoader/Obj_Loader.h
@@ -1,12 +1,7 @@
 #ifndef OBJ_TO_H_INCLUDED
 #define OBJ_TO_H_INCLUDED
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
 #include <iostream>
 #include <fstream>
 #include <vector>
--- objLoader/objLoader.cpp
+++ objLoader/objLoader.cpp
@@ -1,21 +1,12 @@
 ﻿#define GLM_FORCE_RADIANS
 #define GLM_FORCE_SWIZZLE
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+
+
 #include <stdlib.h>
 #include <stdio.h>
-#include "constants.h"
 #include <iostream>
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include <iostream>
 #include <fstream>
